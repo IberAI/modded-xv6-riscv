18d7161a563315757ab3a4c0c0220779302e88d4
IberAI <ilteber.dover@gmail.com>
2026-09-20 18:16:01 -0400

    fixed flake issue

--- flake.nix
+++ flake.nix
@@ -5,7 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs}: let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     systems = [
       "x86_64-linux"
       "aarch64-linux"
